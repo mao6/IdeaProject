71b5bbcd1e77d0a94e7285820f07acd23544852d
aomao <1585281572@qq.com>
2017-06-06 10:43:31 +0800

    first commit!

---
+++ src/com/aomao/entity/Main.java
@@ -0,0 +1,12 @@
+package com.aomao.entity;
+
+/**
+ * Created by aomao on 17-6-1.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("hello java!");
+
+    }
+
+}
